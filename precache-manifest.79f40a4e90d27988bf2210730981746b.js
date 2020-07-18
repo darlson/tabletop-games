d4de8bddf3ed4098088df714c757a7736a60a7e0
darlson <davertiser@gmail.com>
2020-07-18 17:19:49 -0600

    Updates

---
+++ precache-manifest.79f40a4e90d27988bf2210730981746b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4670eeb71d42d0c4df50a149a5f4f7e8",
+    "url": "/tabletop-games/index.html"
+  },
+  {
+    "revision": "160403e717baa2c21794",
+    "url": "/tabletop-games/static/css/main.2a47d261.chunk.css"
+  },
+  {
+    "revision": "1d0b7b804843d4415d73",
+    "url": "/tabletop-games/static/js/2.09279fe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tabletop-games/static/js/2.09279fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160403e717baa2c21794",
+    "url": "/tabletop-games/static/js/main.d65e4136.chunk.js"
+  },
+  {
+    "revision": "75518114cba6e2581700",
+    "url": "/tabletop-games/static/js/runtime-main.61e45bee.js"
+  }
+]);
